de83c5c381ca97342009bc157b91ceaace8fc73d
ggingggang05 <yllee221b@gmail.com>
2024-04-16 10:05:31 +0900

    변수명 수정

--- demo03/src/components/Ex04.js
+++ demo03/src/components/Ex04.js
@@ -4,29 +4,29 @@ import { useState } from "react";
 //function
 function Ex04(){
 
-    const [won, setWon] = useState(0);
+    const [money, setMoney] = useState(0);
 
     return(
         <>
             <h2>은행 이체 화면 문제</h2>
 
-            <hr></hr>
+            <hr/>
 
-            <input type="text" value={won}></input>원
+            <input type="text" value={money} readOnly></input>원
 
             <hr/>
 
-            <button onClick={e=>setWon(won + 1000000)}>100만</button>
+            <button onClick={e=>setMoney(money + 1000000)}>100만</button>
             &nbsp;&nbsp;
-            <button onClick={e=>setWon(won + 500000)}>50만</button>
+            <button onClick={e=>setMoney(money + 500000)}>50만</button>
             &nbsp;&nbsp;
-            <button onClick={e=>setWon(won + 100000)}>10만</button>
+            <button onClick={e=>setMoney(money + 100000)}>10만</button>
             &nbsp;&nbsp;
-            <button onClick={e=>setWon(won + 50000)}>5만</button>
+            <button onClick={e=>setMoney(money + 50000)}>5만</button>
             &nbsp;&nbsp;
-            <button onClick={e=>setWon(won + 10000)}>1만</button>
+            <button onClick={e=>setMoney(money + 10000)}>1만</button>
             &nbsp;&nbsp;
-            <button onClick={e=>setWon(0)}>정정</button>
+            <button onClick={e=>setMoney(0)}>정정</button>
 
         </>
     );
