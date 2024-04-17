7c4b8de95396661cfd78b4e6acad06e6a17c6f51
ggingggang05 <yllee221b@gmail.com>
2024-04-17 14:33:33 +0900

    삭제 효과 내보기

--- demo05/src/components/Ex03.js
+++ demo05/src/components/Ex03.js
@@ -14,26 +14,56 @@ function Ex03(){
         ]
     );
 
+    //전체 삭제란 비어있는 배열로 state를 덮어쓰기 한다는 것을 의미한다
+    //(중요) React는 절대로 state를 고치면 안 된다(새로 만들어 덮어쓰기 해야 함)
+    
+    //function clearItems(e) {}
+    const clearItems = (e) => {
+        setItems([]);//삭제
+    };
+
+    const deleteItem = (target) => {
+        //items에서 item이 아닌 항목만 검색해서 재설정(삭제 효과)
+        // const searchItems = items.filter(function(item){
+        //     return item.itemNo !== target.itemNo;
+        // });
+        const searchItems =
+                items.filter((item)=>item.itemNo !== target.itemNo);
+
+        setItems(searchItems);//검색 결과로 state를 덮어쓰기
+    };
+
     return(
         <>
             <Jumbotron title="예제3번" content="예제 3번일까나?"></Jumbotron>
 
             <div className="row mt-4">
                 <div className="col">
-                    <table className="table table-danger text-center">
+                    {/* <button className="btn btn-danger" onClick={clearItems}>전체삭제</button> */}
+                    <button className="btn btn-danger" onClick={e => clearItems(e)}>전체삭제</button>
+                </div>
+            </div>
+
+            <div className="row mt-4">
+                <div className="col">
+                    <table className="table table-hover text-center">
                         <thead>
                             <tr>
                                 <td>번호</td>
                                 <td>품명</td>
                                 <td>가격</td>
+                                <td>삭제</td>
                             </tr>
                         </thead>
                         <tbody>
-                            {items.map(item => (
+                            {items.map((item) => (
                                 <tr>
                                     <td>{item.itemNo}</td>
                                     <td>{item.itemName}</td>
                                     <td>{item.itemPrice}원</td>
+                                    <td>
+                                        <button className="btn btn-danger" onClick={e=>deleteItem(item)}> &minus; </button>
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
