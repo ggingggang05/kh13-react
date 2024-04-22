17ee631f386225dfaf5d9ccd7a8d80e79bef194e
ggingggang05 <yllee221b@gmail.com>
2024-04-22 14:19:27 +0900

    화면에서 수정하기

--- demo05/src/components/integrated/Student.js
+++ demo05/src/components/integrated/Student.js
@@ -3,6 +3,8 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { HiArchiveBoxXMark } from "react-icons/hi2";
 import axios from "../utils/CustomAxios"; //내가 만든 파일로 임포트 시켜주기
 import { Modal } from 'bootstrap';
+import { FiEdit } from "react-icons/fi";
+import { TbPencilCancel } from "react-icons/tb";
 
 const Student = () => {
 
@@ -103,6 +105,74 @@ const Student = () => {
         });
     }, [input]);
 
+    //해당 줄을 수정상태(edit==false)로 만드는 함수
+    //target은 수정을 누른 줄의 학생 정보
+    const editStudent = useCallback((target)=>{
+        //1, students를 복제한다
+        const copy = [...students];
+        //2, copy를 고친다
+        //- copy 중에서 target과 동일한 정보를 가진 항목을 찾아서 edit:true로 만든다
+        //- 배열을 변환시켜야 하므로 map 함수를 사용한다
+        const copy2 = copy.map(student=>{
+            //target : 수정버튼을 누른 학생정보, student: 현재 회차의 학생정보
+            if(target.studentId === student.studentId){ //target이랑 student가 동일하다면 //원하는 정보일 경우
+                return {
+                    ...student,//나머지 정보는 유지하되
+                    edit:true,//edit 관련된 처리를 추가하여 반환
+                };
+            }
+            else { //원하는 정보가 아닐 경우 - 데이터를 그대로 반환
+                return {...student}; //데이터를 그대로 반환
+            }
+        });
+
+        //3, copy를 students에 덮어쓰기 한다
+        setStudents(copy2);
+    }, [students]);
+
+    const cancelEditStudent = useCallback((target)=>{
+        //1, students를 복제한다
+        const copy = [...students];
+        //2, copy를 고친다
+        //- copy 중에서 target과 동일한 정보를 가진 항목을 찾아서 edit:true로 만든다
+        //- 배열을 변환시켜야 하므로 map 함수를 사용한다
+        const copy2 = copy.map(student=>{
+            //target : 수정버튼을 누른 학생정보, student: 현재 회차의 학생정보
+            if(target.studentId === student.studentId){ //target이랑 student가 동일하다면 //원하는 정보일 경우
+                return {
+                    ...student,//나머지 정보는 유지하되
+                    edit:false,//edit 관련된 처리를 추가하여 반환
+                };
+            }
+            else { //원하는 정보가 아닐 경우 - 데이터를 그대로 반환
+                return {...student}; //데이터를 그대로 반환
+            }
+        });
+
+        //3, copy를 students에 덮어쓰기 한다
+        setStudents(copy2);
+    }, [students]);
+
+    //수정 입력창에서 입력이 발생할 경우 실행할 함수
+    //- students 중에서 대상을 찾아 해당 필드를 교체하여 재설정
+    //- e는 입력이 발생한 창의 이벤트 정보
+    //- target은 입력이 발생한 창이 있는 줄의 학생 정보
+    const changeStudent = useCallback((e, target)=>{
+        const copy = [...students];
+        const copy2 = copy.map(student=>{
+            if(target.studentId === student.studentId) { //이벤트 발생한 학생이라면
+                return {
+                    ...student,//나머지 정보는 유지
+                    [e.target.name] : e.target.value//단, 입력항목만 교체
+                };
+            }
+            else {//다른 학생이라면
+                return {...student};//현상유지
+            }
+        });
+        setStudents(copy2);
+    }, [students]);
+
 
     //ref + modal
     const bsModal = useRef();
@@ -148,15 +218,47 @@ const Student = () => {
                         <tbody className='text-center'>
                             {students.map(student => (
                                 <tr key={student.studentId}>
-                                    <td>{student.studentId}</td>
-                                    <td>{student.name}</td>
-                                    <td>{student.koreanScore}</td>
-                                    <td>{student.englishScore}</td>
-                                    <td>{student.mathScore}</td>
-                                    <td>
-                                        <HiArchiveBoxXMark className='text-danger' onClick={e=>deleteStudent(student)}/>
-                                    </td>
-                                </tr>
+                                    { student.edit === true ? (
+                                        //수정화면
+                                        <>
+                                            <td>{student.studentId}</td>
+                                            <td>
+                                                <input type='text' className='form-control' name='name' 
+                                                            value={student.name} onChange={e =>changeStudent(e, student)}/> 
+                                            </td>
+                                            <td>
+                                                <input type='number' className='form-control' name='koreanScore' 
+                                                            value={student.koreanScore} onChange={e =>changeStudent(e, student)}/> 
+                                            </td>
+                                            <td>
+                                                <input type='number' className='form-control' name='englishScore' 
+                                                            value={student.englishScore} onChange={e =>changeStudent(e, student)}/> 
+                                            </td>
+                                            <td>
+                                                <input type='number' className='form-control' name='mathScore' 
+                                                            value={student.mathScore} onChange={e =>changeStudent(e, student)}/> 
+                                            </td>
+                                            <td>
+                                            <TbPencilCancel className='text-danger' 
+                                                    onClick={e=>cancelEditStudent(student)}/>
+                                            </td>
+                                        </>
+                                    ) : (
+                                        //일반화면
+                                        <>
+                                            <td>{student.studentId}</td>
+                                            <td>{student.name}</td>
+                                            <td>{student.koreanScore}</td>
+                                            <td>{student.englishScore}</td>
+                                            <td>{student.mathScore}</td>
+                                            <td>
+                                                <FiEdit className="text-warning me-2" 
+                                                            onClick={e=>editStudent(student)}/>
+                                                <HiArchiveBoxXMark className='text-danger' onClick={e=>deleteStudent(student)}/>
+                                            </td>
+                                        </>
+                                    ) }        
+                                    </tr>
                             ))}
                         </tbody>
                     </table>
